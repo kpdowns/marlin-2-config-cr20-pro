7810a37bd2eac5c40823243eb18a99597ec1af85
Kevin Downs <kevindwns@gmail.com>
2020-12-12 13:42:31 +0200

    Set offset for 0,0 position to shift printing area to the middle of the build plate

--- Configuration.h
+++ Configuration.h
@@ -1123,8 +1123,8 @@
 #define Y_BED_SIZE 235
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
-#define Y_MIN_POS 0
+#define X_MIN_POS -7.5
+#define Y_MIN_POS -7.5
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
