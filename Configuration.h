0beaf8bd602e22913bc043909a0dd6b4729f8b64
Kevin Downs <kevindwns@gmail.com>
2020-12-12 13:43:57 +0200

    Set nozzle offset to match physical dimensions on printer

--- Configuration.h
+++ Configuration.h
@@ -983,7 +983,8 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -43, -5, 0 }
+
+#define NOZZLE_TO_PROBE_OFFSET { -46, -6, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
